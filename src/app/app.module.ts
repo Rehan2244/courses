4ce7f4697408297f9d4f6c26bdc4172ba0cc1825
Rehan2244 <rehanskillet@gmail.com>
2023-12-28 22:52:12 +0530

    fixed all animation issue rose after update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,10 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from './landing-page/container/navbar/navbar.component';
-import {MatLegacyButtonModule as MatButtonModule} from '@angular/material/legacy-button';
+import {MatButtonModule} from '@angular/material/button';
 import { CheckoutComponent } from './checkout/checkout.component'
-import { MatLegacyFormFieldModule as MatFormFieldModule } from '@angular/material/legacy-form-field';
-import { MatLegacyInputModule as MatInputModule } from '@angular/material/legacy-input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { ToastrModule } from 'ngx-toastr';
 
--- src/app/landing-page/landing-page.module.ts
+++ src/app/landing-page/landing-page.module.ts
@@ -4,16 +4,16 @@ import { CommonModule } from '@angular/common';
 import { LandingPageRoutingModule } from './landing-page-routing.module';
 import { LandingPageComponent } from './landing-page.component';
 import { MatIconModule } from '@angular/material/icon';
-import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button';
+import { MatButtonModule } from '@angular/material/button';
 import { MatBadgeModule } from '@angular/material/badge';
 import { CoursesComponent } from './container/courses/courses.component';
 import { WishlistComponent } from './container/wishlist/wishlist.component';
 import { CartComponent } from './container/cart/cart.component';
 import { NavbarComponent } from './container/navbar/navbar.component';
 import { CourseCardComponent } from './container/course-card/course-card.component';
-import {MatLegacyFormFieldModule as MatFormFieldModule, MatLegacyLabel as MatLabel} from '@angular/material/legacy-form-field'
-import {MatLegacyInputModule as MatInputModule} from '@angular/material/legacy-input';
-import {MatLegacyPaginatorModule as MatPaginatorModule} from '@angular/material/legacy-paginator';
+import { MatFormFieldModule,MatLabel} from '@angular/material/form-field'
+import {MatInputModule} from '@angular/material/input';
+import {MatPaginatorModule} from '@angular/material/paginator';
 import { ProfileComponent } from './container/profile/profile.component'
 import { FormsModule } from '@angular/forms';
 import { ProductDetailComponent } from './container/product-detail/product-detail.component';
